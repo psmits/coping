c4d89b6f4950b2c05e65eb015abcfeee73812b3e
psmits <thestinkypete@gmail.com>
2015-12-01 23:34:45 -0600

    temperature data in

---
+++ R/mung_clim.r
@@ -0,0 +1,47 @@
+# process the "climate" information
+zac <- read.csv('../data/2008_zachos_data.csv', stringsAsFactors = FALSE)
+
+names(zac) <- c('loc', 'age', 'genus', 'o18', 'c13', 
+                'o18.5pt', 'c13.5pt', 'comment')
+zac <- zac[zac$age <= (max(occur$bins)), ]
+zac <- zac[!is.na(zac$o18), ]
+b <- unique(occur$bins)
+b <- as.matrix(cbind(b - 2, b))
+isotope <- list()
+zac.cohort <- array(NA, dim = nrow(zac))
+for(ii in seq(max(occur$bins) / 2)) {
+  isotope[[ii]] <- zac$o18[zac$age > b[ii, 1] & zac$age <= b[ii, 2]]
+  zac.cohort[(zac$age > b[ii, 1] & zac$age <= b[ii, 2])] <- ii
+}
+zac <- zac[!is.na(zac.cohort), ]
+zac.cohort <- zac.cohort[!is.na(zac.cohort)]
+mean.o18 <- laply(split(zac$o18, zac.cohort), mean)
+mean.o18 <- arm::rescale(mean.o18)
+range.o18 <- laply(split(zac$o18, zac.cohort), function(x) 
+                   abs(quantile(x, 0.25) - quantile(x, 0.75)))
+range.o18 <- arm::rescale(range.o18)
+
+
+# how about the mg/ca data...
+cram <- read.delim('../data/cramer/cramer_mgca_pref.txt', sep = '\t')
+cram.temp <- read.delim('../data/cramer/cramer_temp.txt', sep = '\t')
+temp.est <- cram.temp$Temperature
+temp.range <- cram.temp$Temperature.max - cram.temp$Temperature.min
+
+b <- unique(occur$bins)
+b <- as.matrix(cbind(b - 2, b))
+temp.time.mean <- list()
+temp.time.range <- list()
+for(ii in seq(max(occur$bins) / 2)) {
+  temp.time.mean[[ii]] <- temp.est[cram.temp$Age > b[ii, 1] & 
+                                   cram.temp$Age <= b[ii, 2]]
+  temp.time.range[[ii]] <- temp.range[cram.temp$Age > b[ii, 1] & 
+                                      cram.temp$Age <= b[ii, 2]]
+}
+#temp.time.mean <- laply(temp.time.mean, function(x) mean(x, na.rm = TRUE))
+temp.time.mean <- arm::rescale(laply(temp.time.mean, function(x) 
+                                     mean(x, na.rm = TRUE)))
+temp.time.range <- arm::rescale(laply(temp.time.range, function(x) 
+                                      mean(x, na.rm = TRUE)))
+#plot(cram.temp$Age, cram.temp$Temperature)
+#points(seq(length(temp.time.mean)) * 2, temp.time.mean, col = 'blue')
--- R/trait_setup.r
+++ R/trait_setup.r
@@ -13,7 +13,6 @@ load('../data/scaled_super.rdata')
 load('../data/body_mass.rdata')
 
 dat <- read.csv('../data/mam-occs.csv', stringsAsFactors = FALSE)
-zac <- read.csv('../data/2008_zachos_data.csv', stringsAsFactors = FALSE)
 
 occur <- clean.occurrence(dat)
 ss <- split(occur, occur$bins)
@@ -30,32 +29,16 @@ occur <- occur[!(occur$name.bi %in%
 spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
 
 # for testing purposes
-#keep <- createDataPartition(occur$bins, p = 0.4)
-#keepname <- str_replace(unique(occur$name.bi[keep[[1]]]), ' ', '_')
-#hot.fix <- name.check(spt, data.names = keepname)
-#spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
-#occur <- occur[keep[[1]], ]
+keep <- createDataPartition(occur$bins, p = 0.4)
+keepname <- str_replace(unique(occur$name.bi[keep[[1]]]), ' ', '_')
+hot.fix <- name.check(spt, data.names = keepname)
+spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
+occur <- occur[keep[[1]], ]
 
-# process the "climate" information
-names(zac) <- c('loc', 'age', 'genus', 'o18', 'c13', 
-                'o18.5pt', 'c13.5pt', 'comment')
-zac <- zac[zac$age <= (max(occur$bins)), ]
-zac <- zac[!is.na(zac$o18), ]
-b <- unique(occur$bins)
-b <- as.matrix(cbind(b - 2, b))
-isotope <- list()
-zac.cohort <- array(NA, dim = nrow(zac))
-for(ii in seq(max(occur$bins) / 2)) {
-  isotope[[ii]] <- zac$o18[zac$age > b[ii, 1] & zac$age <= b[ii, 2]]
-  zac.cohort[(zac$age > b[ii, 1] & zac$age <= b[ii, 2])] <- ii
-}
-zac <- zac[!is.na(zac.cohort), ]
-zac.cohort <- zac.cohort[!is.na(zac.cohort)]
-mean.o18 <- laply(split(zac$o18, zac.cohort), mean)
-mean.o18 <- arm::rescale(mean.o18)
-range.o18 <- laply(split(zac$o18, zac.cohort), function(x) 
-                   abs(quantile(x, 0.25) - quantile(x, 0.75)))
-range.o18 <- arm::rescale(range.o18)
+
+# process climate information
+source('../R/mung_clim.r')
+
 
 # split by genus to make summaries just in case
 bygen <- split(occur, occur$occurrence.genus_name)
@@ -92,6 +75,9 @@ cohort <- mapvalues(cohort, from = unique(cohort), to = seq(C))
 #id <- as.numeric(as.factor(occur$name.bi))
 isoval <- mean.o18
 isorang <- range.o18
+### WARNING to include need to remove oldest bin!!!!
+# tempval <- temp.time.mean
+# isorang <- temp.time.range
 
 stan_rdump(list = c('K', 'N', 'D', 'C', 'y', 'x', 
                     'cohort', 'isoval', 'isorang'),
