cb9eccb7f09ee8f8f21c0da251e8509f0090b270
psmits <thestinkypete@gmail.com>
2017-03-05 12:41:11 -0600

    re-ran analyses because why not

--- src/home_wrap.sh
+++ src/home_wrap.sh
@@ -1,12 +1,12 @@
-#../stan/turnover_revamp variational \
-#  id=1 \
-#  init=0 \
-#  data file=../data/data_dump/trait_w_gaps.data.R \
-#  output file=../data/mcmc_out/turnover_revamp_1_advi.csv &
-#wait
-../stan/turnover_revamp_2 variational \
+../stan/turnover_revamp variational \
   id=1 \
   init=0 \
   data file=../data/data_dump/trait_w_gaps.data.R \
-  output file=../data/mcmc_out/turnover_revamp_2_advi.csv &
+  output file=../data/mcmc_out/turnover_revamp_1_advi.csv &
 wait
+#../stan/turnover_revamp_2 variational \
+#  id=1 \
+#  init=0 \
+#  data file=../data/data_dump/trait_w_gaps.data.R \
+#  output file=../data/mcmc_out/turnover_revamp_2_advi.csv &
+#wait
