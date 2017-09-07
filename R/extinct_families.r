02ac5c9b2c60ac465221de1b429158a8242b084b
psmits <thestinkypete@gmail.com>
2017-09-07 15:46:27 -0700

    taxonomy sorting and fixing functions work. order ready as factor for model.

--- R/extinct_families.r
+++ R/extinct_families.r
@@ -1,35 +1,53 @@
+extinct <- list()
+
+# problem genera in families
+#viverravidae <- 
+miacidae <- c('Oodectes', 'Vulpavus', 'Tapocyon', 'Procynodictis', 'Uintacyon', 'Vassacyon', 'Gracilocyon')
+erinaceomorpha <- c('Talpavus', 'Diacodon', 'Talpavoides', 'Diacocherus', 'Aethomylos')
+homacodontidae <- c('Antiacodon')
+
+extinct[[1]] <- list(Miacidae = miacidae,
+                     Erinaceomorpha = erinaceomorpha,
+                     Homacodontidae = homacodontidae)
+
+
+# familes in orders
 cingulata <- c('Pampatheriidae', 'Glyptodontidae')
-pilosa <- c('Mylodontidae', 'Nothrotheriidae', 'Megatheriidae')
+pilosa <- c('Mylodontidae', 'Nothrotheriidae', 'Megatheriidae', 'Megalonychidae')
 notoungulata <- c('Toxodontidae')
-soricomorpha <- c('Plesiosoricidae', 'Proscalopidae', 'Micropternodontidae', 'Apternodontidae')
+soricomorpha <- c('Plesiosoricidae', 'Proscalopidae', 'Micropternodontidae', 'Apternodontidae', 'Parpaternodontidae')
 lipotyphla <- c('Geolabididae')
 creodonta <- c('Hyaenodontidae', 'Oxyaenidae')
 leptictida <- c('Didymoconidae', 'Gypsonictopidae', 'Kulbeckiidae', 'Leptictidae', 'Pseudorhyncocyonidae')
-cimolesta <- c('Apatemyidae', 'Palaeoryctidae', 'Esthonychidae', 'Coryphodontidae', 'Pentacodontidae', 'Barylambdidae', 'Cimolestidae', 'Cyriacotheriidae', 'Titanoideidae', 'Pantolambdidae', 'Pantolestidae')
-eulipotyphla <- c('Nyctitheriidae')
+cimolesta <- c('Apatemyidae', 'Palaeoryctidae', 'Esthonychidae', 'Coryphodontidae', 'Pentacodontidae', 'Barylambdidae', 'Cimolestidae', 'Cyriacotheriidae', 'Titanoideidae', 'Pantolambdidae', 'Pantolestidae', 'Stylinodontidae', 'Conoryctidae')
+eulipotyphla <- c('Nyctitheriidae', 'Erinaceomorpha', 'Soricidae', 'Heterosoricidae')
 multituberculata <- c('Neoplagiaulacidae', 'Microcosmodontidae', 'Ptilodontidae', 'Cimolodontidae', 'Taeniolabididae', 'Eucosmodontidae')
 dermoptera <- c('Plagiomenidae', 'Mixodectidae')
 pholidota <- c('Epoicotheriidae', 'Metacheiromyidae', 'Escavadodontidae')
 mesonychia <- c('Mesonychidae', 'Hapalodectidae', 'Triisodontidae')
 condylarthra <- c('Hyopsodontidae', 'Phenacodontidae', 'Arctocyonidae', 'Periptychidae', 'Chriacidae')
 dinocerata <- c('Uintatheriidae', 'Prodinoceratidae')
-miacoidae <- c('Viverravidae', 'Miacidae')
-artiodactyla <- c('Gelocidae', 'Protoceratidae', 'Merycoidodontidae', 'Palaeomerycidae', 'Leptomerycidae', 'Anthracotheriidae', 'Entelodontidae', 'Hypertragulidae', 'Agriochoeridae', 'Leptochoeridae', 'Oromerycidae', 'Dichobunidae', 'Achaenodontidae')
+carnivora <- c('Viverravidae', 'Miacidae')
+artiodactyla <- c('Gelocidae', 'Protoceratidae', 'Merycoidodontidae', 'Palaeomerycidae', 'Leptomerycidae', 'Anthracotheriidae', 'Entelodontidae', 'Hypertragulidae', 'Agriochoeridae', 'Leptochoeridae', 'Oromerycidae', 'Dichobunidae', 'Achaenodontidae', 'Homacodontidae')
+didelphimorphia <- c('Herpetotheriidae', 'Peradectidae')
+acreodi <- c('Triisodontidae', 'Mesonychidae', 'Hapalodectidae')
 
-extinct <- list(Cingulata = cingulata, 
-                Pilosa = pilosa,
-                Notoungulata = notoungulata,
-                Soricomorpha = soricomorpha,
-                Lipotyphla = lipotyphla,
-                Creodonta = creodonta,
-                Leptictida = leptictida,
-                Cimolesta = cimolesta,
-                Eulipotyphla = eulipotyphla,
-                Multituberculata = multituberculata,
-                Dermoptera = dermoptera,
-                Pholidota = pholidota,
-                Mesonychia = mesonychia,
-                Condylarthra = condylarthra,
-                Dinocerata = dinocerata,
-                Miacoidae = miacoidae,
-                Artiodactyla = artiodactyla)
+extinct[[2]] <- list(Cingulata = cingulata, 
+                     Pilosa = pilosa,
+                     Notoungulata = notoungulata,
+                     Soricomorpha = soricomorpha,
+                     Lipotyphla = lipotyphla,
+                     Creodonta = creodonta,
+                     Leptictida = leptictida,
+                     Cimolesta = cimolesta,
+                     Eulipotyphla = eulipotyphla,
+                     Multituberculata = multituberculata,
+                     Dermoptera = dermoptera,
+                     Pholidota = pholidota,
+                     Mesonychia = mesonychia,
+                     Condylarthra = condylarthra,
+                     Dinocerata = dinocerata,
+                     Carnivora = carnivora,
+                     Artiodactyla = artiodactyla,
+                     Didelphimorphia = didelphimorphia,
+                     Acreodi = acreodi)
--- R/taxonomy.r
+++ R/taxonomy.r
@@ -1,88 +1,82 @@
-library(taxize)
-library(plyr)
-library(stringr)
+update.taxonomy.eol <- function(occur, key) {
+  # get away from the input asap
+  tt <- occur
+  #tt <- occur[, c('order', 'family', 'genus')]
+  
+  # these parts are slow, use uniques and then put back in
+  gen <- unique(tt[, 3])
+  #eol.id <- llply(gen, function(x) try(get_eolid(x, key = key, rows = 1)))
+  eol.id <- mclapply(gen, function(x) try(get_eolid(x, key = key, rows = 1)), 
+                     mc.cores = detectCores())
+    # there can be strange errors surrounding some taxa
+    # by using try, i at least always get output
+  #eol.class <- llply(eol.id, function(x) try(classification(x, key = key)))
+  eol.class <- mclapply(eol.id, function(x) try(classification(x, key = key)), 
+                        mc.cores = detectCores())
 
-source('../R/my_get_eolid.r')
-source('../R/eol_taxonomy.r')
-source('../R/taxon_names.r')
-source('../R/extinct_families.r')
-
-eol.key = '2a9932f264f3f0421db36158b6e785b535c6da0e'
-
-
-# update from the now
-miss.fam <- dat$occurrence.genus_name[dat$family_name == '']
-in.now <- unique(miss.fam[miss.fam %in% now$GENUS])
-now.info <- now[now$GENUS %in% in.now, ]
-now.info <- now.info[now.info$FAMILY != 'incertae sedis', ]
-now.heir <- unique(now.info[, c('ORDER', 'FAMILY', 'GENUS')])
-dat <- replace.taxonomy(dat, now.heir)
-
-miss.fam <- eur$occurrence.genus_name[eur$family_name == '']
-in.now <- unique(miss.fam[miss.fam %in% now$GENUS])
-now.info <- now[now$GENUS %in% in.now, ]
-now.info <- now.info[now.info$FAMILY != 'incertae sedis', ]
-now.heir <- unique(now.info[, c('ORDER', 'FAMILY', 'GENUS')])
-eur <- replace.taxonomy(eur, now.heir)
-
-# update from Susumu
-miss.fam <- dat$occurrence.genus_name[dat$family_name == '']
-in.tom <- unique(miss.fam[miss.fam %in% tomiya$Genus])
-tom.info <- tomiya[tomiya$Genus %in% in.tom, ]
-tom.info <- tom.info[tom.info$Family != 'unknown', ]
-tom.heir <- unique(tom.info[, c('Order', 'Family', 'Genus')])
-dat <- replace.taxonomy(dat, tom.heir)
-
-miss.fam <- eur$occurrence.genus_name[eur$family_name == '']
-in.tom <- unique(miss.fam[miss.fam %in% tomiya$Genus])
-tom.info <- tomiya[tomiya$Genus %in% in.tom, ]
-tom.info <- tom.info[tom.info$Family != 'unknown', ]
-tom.heir <- unique(tom.info[, c('Order', 'Family', 'Genus')])
-eur <- replace.taxonomy(eur, tom.heir)
-
-
-# update with EOL
-genera <- unique(c(dat$occurrence.genus_name, eur$occurrence.genus_name))
-genera <- genera[genera != 'Vulpes']
-heir <- grab.heir(genera, key = eol.key)  # updated hierarchy information
-orders <- col_downstream(name = 'Mammalia', downto = 'Order')
-new.orders <- c('Eulipotyphla', # combines erinaceidae and soricomorpha
-                'Cetartiodactyla' # combines cetacea and artiodactyla
-                )
-orders <- c(orders$Mammalia[, 2], new.orders)
-heir <- heir[(heir[, 1] %in% orders), ]  # there are random plants/insects
-# for matching genera, update family and order information
-udat <- replace.taxonomy(dat, heir)
-ueur <- replace.taxonomy(eur, heir)
-udat <- miss.ord(udat)
-ueur <- miss.ord(ueur)
-
-
-# extinct families
-extinct.heir <- Reduce(rbind, Map(function(x, y) cbind(order = rep(x, length(y)), 
-                                                       family = y),
-                                  x = names(extinct), y = extinct))
-bonus.ord <- unique(c(orders, names(extinct)))
-
-replace.extinct <- function(data, heir) {
-  for(ii in seq(nrow(heir))) {
-    rr <- which(data$family_name == heir[ii, 2])
-    data[rr, 'order_name'] <- heir[ii, 1]
+  # get the order, family information from those taxa found
+  out <- llply(eol.class, function(x) {
+                 if(class(x[[1]]) == 'data.frame') {
+                   # make sure it is a mammal
+                   if(x[[1]][x[[1]]$rank == 'class', 1] == 'Mammalia') {
+                     a <- x[[1]]$rank %in% c('order', 'family')
+                     b <- x[[1]][a, ]
+                     b[, 1]
+                   }
+                 } else {  # bad responses incl. errors
+                   c(NA, NA)
+                 }})
+  out <- Reduce(rbind, out)
+  out <- cbind(out, gen)
+ 
+  # everything is good till about here
+  # sometimes i haven't learned more
+  no.new <- apply(out[, 1:2], 2, is.na)
+  for(ii in seq(nrow(out))) {
+    if(!any(is.na(out[ii, ]))) {
+      upd <- tt[, 3] %in% out[ii, 3]
+      tt[upd, 1:2] <- out[ii, 1:2]
+    }
   }
-  data$order_name[data$order_name == 'Acreodi'] <- 'Mesonychia'
-  data
+
+  tt
 }
-udat <- replace.extinct(udat, extinct.heir)
-ueur <- replace.extinct(ueur, extinct.heir)
-udat$family_name[grep('[0-9]', udat$family_name)] <- ''
-udat$order_name[grep('[0-9]', udat$order_name)] <- ''
-ueur$family_name[grep('[0-9]', ueur$family_name)] <- ''
-ueur$order_name[grep('[0-9]', ueur$order_name)] <- ''
 
-keep <- c('order_name', 'family_name', 'occurrence.genus_name', 'name.bi', 
-          'ma_min', 'ma_max')
-na.tax <- udat[, keep]
-er.tax <- ueur[, keep]
+# not everything is on EOL
+# especially extinct things
+# two stage process
+#   put genera in families
+#   put families in orders
+update.taxonomy.extinct <- function(occur, extinct) {
+  # get away from the input asap
+  tt <- occur
+  #tt <- occur[, c('order', 'family', 'genus')]
+ 
+  # genera in families
+  ext.match <- llply(extinct[[1]], function(x) tt[, 3] %in% x)
+  
+  for(ii in seq(length(ext.match))) {
+    if(any(ext.match[[ii]])) {
+      tt[ext.match[[ii]], 2] <- names(extinct[[1]][ii])
+    }
+  }
 
-save(na.tax, er.tax, 
-     file = '../data/update_taxonomy.rdata')
+
+  # families in orders
+  # if the family is extinct and i know it, give it an order
+  ext.match <- llply(extinct[[2]], function(x) tt[, 2] %in% x)
+  
+  for(ii in seq(length(ext.match))) {
+    if(any(ext.match[[ii]])) {
+      tt[ext.match[[ii]], 1] <- names(extinct[[2]][ii])
+    }
+  }
+
+  tt
+}
+
+
+
+# i need to consolidate orders
+#   eg Lypotiphila == Eulypotiphila
+#
