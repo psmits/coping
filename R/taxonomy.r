c1a6e2e8760b8bd2122205b81c6dc7938ca08ee2
psmits <thestinkypete@gmail.com>
2017-09-07 09:26:20 -0700

    started digging into fixing taxonomy issues

---
+++ R/taxonomy.r
@@ -0,0 +1,88 @@
+library(taxize)
+library(plyr)
+library(stringr)
+
+source('../R/my_get_eolid.r')
+source('../R/eol_taxonomy.r')
+source('../R/taxon_names.r')
+source('../R/extinct_families.r')
+
+eol.key = '2a9932f264f3f0421db36158b6e785b535c6da0e'
+
+
+# update from the now
+miss.fam <- dat$occurrence.genus_name[dat$family_name == '']
+in.now <- unique(miss.fam[miss.fam %in% now$GENUS])
+now.info <- now[now$GENUS %in% in.now, ]
+now.info <- now.info[now.info$FAMILY != 'incertae sedis', ]
+now.heir <- unique(now.info[, c('ORDER', 'FAMILY', 'GENUS')])
+dat <- replace.taxonomy(dat, now.heir)
+
+miss.fam <- eur$occurrence.genus_name[eur$family_name == '']
+in.now <- unique(miss.fam[miss.fam %in% now$GENUS])
+now.info <- now[now$GENUS %in% in.now, ]
+now.info <- now.info[now.info$FAMILY != 'incertae sedis', ]
+now.heir <- unique(now.info[, c('ORDER', 'FAMILY', 'GENUS')])
+eur <- replace.taxonomy(eur, now.heir)
+
+# update from Susumu
+miss.fam <- dat$occurrence.genus_name[dat$family_name == '']
+in.tom <- unique(miss.fam[miss.fam %in% tomiya$Genus])
+tom.info <- tomiya[tomiya$Genus %in% in.tom, ]
+tom.info <- tom.info[tom.info$Family != 'unknown', ]
+tom.heir <- unique(tom.info[, c('Order', 'Family', 'Genus')])
+dat <- replace.taxonomy(dat, tom.heir)
+
+miss.fam <- eur$occurrence.genus_name[eur$family_name == '']
+in.tom <- unique(miss.fam[miss.fam %in% tomiya$Genus])
+tom.info <- tomiya[tomiya$Genus %in% in.tom, ]
+tom.info <- tom.info[tom.info$Family != 'unknown', ]
+tom.heir <- unique(tom.info[, c('Order', 'Family', 'Genus')])
+eur <- replace.taxonomy(eur, tom.heir)
+
+
+# update with EOL
+genera <- unique(c(dat$occurrence.genus_name, eur$occurrence.genus_name))
+genera <- genera[genera != 'Vulpes']
+heir <- grab.heir(genera, key = eol.key)  # updated hierarchy information
+orders <- col_downstream(name = 'Mammalia', downto = 'Order')
+new.orders <- c('Eulipotyphla', # combines erinaceidae and soricomorpha
+                'Cetartiodactyla' # combines cetacea and artiodactyla
+                )
+orders <- c(orders$Mammalia[, 2], new.orders)
+heir <- heir[(heir[, 1] %in% orders), ]  # there are random plants/insects
+# for matching genera, update family and order information
+udat <- replace.taxonomy(dat, heir)
+ueur <- replace.taxonomy(eur, heir)
+udat <- miss.ord(udat)
+ueur <- miss.ord(ueur)
+
+
+# extinct families
+extinct.heir <- Reduce(rbind, Map(function(x, y) cbind(order = rep(x, length(y)), 
+                                                       family = y),
+                                  x = names(extinct), y = extinct))
+bonus.ord <- unique(c(orders, names(extinct)))
+
+replace.extinct <- function(data, heir) {
+  for(ii in seq(nrow(heir))) {
+    rr <- which(data$family_name == heir[ii, 2])
+    data[rr, 'order_name'] <- heir[ii, 1]
+  }
+  data$order_name[data$order_name == 'Acreodi'] <- 'Mesonychia'
+  data
+}
+udat <- replace.extinct(udat, extinct.heir)
+ueur <- replace.extinct(ueur, extinct.heir)
+udat$family_name[grep('[0-9]', udat$family_name)] <- ''
+udat$order_name[grep('[0-9]', udat$order_name)] <- ''
+ueur$family_name[grep('[0-9]', ueur$family_name)] <- ''
+ueur$order_name[grep('[0-9]', ueur$order_name)] <- ''
+
+keep <- c('order_name', 'family_name', 'occurrence.genus_name', 'name.bi', 
+          'ma_min', 'ma_max')
+na.tax <- udat[, keep]
+er.tax <- ueur[, keep]
+
+save(na.tax, er.tax, 
+     file = '../data/update_taxonomy.rdata')
--- R/trait_setup.r
+++ R/trait_setup.r
@@ -6,11 +6,15 @@ library(arm)
 library(ape)
 library(geiger)
 library(caret)
+library(taxize)
 
 source('../R/mung.r')
+source('../R/extinct_families.r')
 
 load('../data/scaled_super.rdata')
 load('../data/body_mass.rdata')
+
+eol.key = '2a9932f264f3f0421db36158b6e785b535c6da0e'
 
 TESTING.x <- FALSE
 TESTING.u <- FALSE
@@ -27,143 +31,215 @@ occur <- clean.occurrence(dat)
 ss <- split(occur, occur$bins)
 occur <- Reduce(rbind, llply(ss, function(x) x[duplicated(x$name.bi), ]))
 
-load('../data/update_taxonomy.rdata')
-na.tax <- na.tax[!(duplicated(na.tax$name.bi)), ]
-fx <- na.tax$name.bi %in% occur$name.bi[occur$comlife == 'ground dwelling']
-na.fx <- na.tax[fx, ]
-occur[match(na.fx$name.bi, occur$name.bi), 
-      c('order', 'family')] <- na.fx[, 1:2]
 
-stance.group <- split(posture$taxon, posture$stance)
-for(ii in seq(length(stance.group))) {
-  mm <- occur$family %in% stance.group[[ii]]
-  gd <- occur$comlife == 'ground dwelling'
-  occur$comlife[mm & gd] <- names(stance.group)[ii]
+# need to do more to taxonomy
+# make a clean taxonomy function
 
-  mm <- occur$order %in% stance.group[[ii]]
-  gd <- occur$comlife == 'ground dwelling'
-  occur$comlife[mm & gd] <- names(stance.group)[ii] 
+# hot fix because genera in parens...
+fix.gen <- str_extract(occur$genus, '(?<=\\().+?(?=\\))') # fucking magic
+occur$genus[!is.na(fix.gen)] <- fix.gen[!is.na(fix.gen)]
+
+# function that...
+#   hunts taxonomy online
+#   replaces the pbdb taxonomy with new info
+#   makes it internally consistent
+# the goal of this is to get orders for as many taxa as possible
+# TODO
+update.taxonomy <- function(occur, key, extinct) {
+  # get away from the input asap
+  tt <- occur[1:100, c('order', 'family', 'genus')]
+  
+  # these parts are slow, use uniques and then put back in
+  gen <- unique(tt[, 3])
+  eol.id <- get_eolid(gen, key = key, rows = 1)  # req. internet
+  eol.class <- classification(eol.id, key = key)  # req. internet
+
+  # get the order, family information from those taxa found
+  out <- llply(eol.class, function(x) {
+                 if(class(x) == 'data.frame') {
+                   a <- x$rank %in% c('order', 'family')
+                   b <- x[a, ]
+                   b[, 1]
+                 } else {  # bad responses
+                   NA
+                 }})
+  out <- Reduce(rbind, out)
+  
+ 
+  # everything is good till about here
+  # sometimes i haven't learned more
+  # pick up here
+  no.new <- apply(out, 2, is.na)
+  for(ii in seq(nrow(no.new))) {
+    if(any(!no.new[ii, ])) {
+      tt[ii, 1:2] <- out[which(gen %in% tt[ii, 3]), ]
+    }
+  }
+
+  # if the family is extinct and i know it, give it an order
+  mat <- list()
+  for(ii in seq(length(extinct)))
+    mat[[ii]] <- tt[, 2] %in% extinct[[1]]
+  
+
+
 }
 
-occur <- occur[occur$comlife != 'ground dwelling', ]
+#update.taxonomy(occur[1:10, ], eol.key, extinct = extinct)
 
 
-# shrink data to match all inputs
-#   this would be the opportunity for setting up an imputation step
-na.mass$name <- str_replace(na.mass[, 1], ' ', '_')
-occur <- occur[occur$name.bi %in% na.mass$name, ]
-occur$mass <- na.mass[match(occur$name.bi, na.mass$name), 2]
+orders <- col_downstream(name = 'Mammalia', downto = 'Order')
 
-## make data and tree match
-#name <- matrix(str_replace(occur$name.bi, ' ', '_'), ncol = 1)
-#hot.fix <- name.check(spt, data.names = name)
-#occur <- occur[!(occur$name.bi %in% 
-#                 str_replace(hot.fix$data_not_tree, '_', ' ')), ]
-#spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
+# awful hardcoded
+new.orders <- c('Eulipotyphla', # combines erinaceidae and soricomorpha
+                'Cetartiodactyla' # combines cetacea and artiodactyla
+                )
 
-## for testing purposes
-#keep <- createDataPartition(occur$bins, p = 0.2)
-#keepname <- str_replace(unique(occur$name.bi[keep[[1]]]), ' ', '_')
-#hot.fix <- name.check(spt, data.names = keepname)
-#spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
-#occur <- occur[keep[[1]], ]
-
-# process climate information
-source('../R/mung_clim.r')
+orders <- c(orders$Mammalia[, 2], new.orders)
 
 
-
-
-occur <- occur[occur$bins != 66, ]
-
-# save true bins
-occur$true.bin <- occur$bins
-# make easy bins
-occur$bins <- occur$bins / 2
-
-
-# !!! makes everything genus level !!!
-# need to make the things work
-by.tax <- split(occur, occur$name.bi)
-#by.tax <- split(occur, occur$genus)
-
-# cols go from younger to older
-sight <- matrix(0, nrow = length(by.tax), ncol = max(occur$bins))
-for(ii in seq(length(by.tax))) {
-  sight[ii, (by.tax[[ii]]$bins)] <- 1
-}
-sight <- sight[, -1]
-
-
-N <- length(by.tax)
-diet <- laply(by.tax, function(x) names(which.max(table(x$comdiet))))
-life <- laply(by.tax, function(x) names(which.max(table(x$comlife))))
-mass <- arm::rescale(log(laply(by.tax, function(x) mean(x$mass))))
-
-# covariates
-diet <- factor(diet, levels = unique(diet)[c(4, 1:3)])
-life <- factor(life, levels = unique(life))
-inter <- interaction(diet, life, drop = TRUE)
-inter <- as.character(inter)
-
-rms <- which(inter == names(which(table(inter) == 1)))
-sight <- sight[-rms, ]
-
-x <- stats::model.matrix( ~ mass + inter)
-x <- x[-rms, ]
-mass <- mass[-rms]
-#x <- x[, colSums(x) != 0]  
-# i think this is the correct thing to do because certain inter not obs
-D <- ncol(x)
-name.name <- colnames(x)
-
-# phylogenetic data
-#vcv <- vcv(spt)
-#vcv <- vcv / max(diag(vcv))
-#U <- nrow(vcv)
-#id <- as.numeric(as.factor(occur$name.bi))
-
-# climate data
-#u <- cbind(mean.o18, range.o18)
+#load('../data/update_taxonomy.rdata')
+#na.tax <- na.tax[!(duplicated(na.tax$name.bi)), ]
+#fx <- na.tax$name.bi %in% occur$name.bi[occur$comlife == 'ground dwelling']
+#na.fx <- na.tax[fx, ]
+#occur[match(na.fx$name.bi, occur$name.bi), 
+#      c('order', 'family')] <- na.fx[, 1:2]
+#
+#
+#stance.group <- split(posture$taxon, posture$stance)
+#for(ii in seq(length(stance.group))) {
+#  mm <- occur$family %in% stance.group[[ii]]
+#  gd <- occur$comlife == 'ground dwelling'
+#  occur$comlife[mm & gd] <- names(stance.group)[ii]
+#
+#  mm <- occur$order %in% stance.group[[ii]]
+#  gd <- occur$comlife == 'ground dwelling'
+#  occur$comlife[mm & gd] <- names(stance.group)[ii] 
+#}
+#
+#occur <- occur[occur$comlife != 'ground dwelling', ]
+#
+#
+## shrink data to match all inputs
+##   this would be the opportunity for setting up an imputation step
+#na.mass$name <- str_replace(na.mass[, 1], ' ', '_')
+#occur <- occur[occur$name.bi %in% na.mass$name, ]
+#occur$mass <- na.mass[match(occur$name.bi, na.mass$name), 2]
+#
+#
+##unique(occur$order)
+##unique(occur$family)
+##unique(occur$family[occur$order == '' & occur$family != ''])
+#
+#
+#
+### make data and tree match
+##name <- matrix(str_replace(occur$name.bi, ' ', '_'), ncol = 1)
+##hot.fix <- name.check(spt, data.names = name)
+##occur <- occur[!(occur$name.bi %in% 
+##                 str_replace(hot.fix$data_not_tree, '_', ' ')), ]
+##spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
+#
+### for testing purposes
+##keep <- createDataPartition(occur$bins, p = 0.2)
+##keepname <- str_replace(unique(occur$name.bi[keep[[1]]]), ' ', '_')
+##hot.fix <- name.check(spt, data.names = keepname)
+##spt <- ape::drop.tip(spt, hot.fix$tree_not_data)
+##occur <- occur[keep[[1]], ]
+#
+## process climate information
+#source('../R/mung_clim.r')
+#
+#occur <- occur[occur$bins != 66, ]
+#
+## save true bins
+#occur$true.bin <- occur$bins
+## make easy bins
+#occur$bins <- occur$bins / 2
+#
+#
+## !!! makes everything genus level !!!
+## need to make the things work
+#by.tax <- split(occur, occur$name.bi)
+##by.tax <- split(occur, occur$genus)
+#
+## cols go from younger to older
+#sight <- matrix(0, nrow = length(by.tax), ncol = max(occur$bins))
+#for(ii in seq(length(by.tax))) {
+#  sight[ii, (by.tax[[ii]]$bins)] <- 1
+#}
+#sight <- sight[, -1]
+#
+#
+#N <- length(by.tax)
+#diet <- laply(by.tax, function(x) names(which.max(table(x$comdiet))))
+#life <- laply(by.tax, function(x) names(which.max(table(x$comlife))))
+#mass <- arm::rescale(log(laply(by.tax, function(x) mean(x$mass))))
+#
+## covariates
+#diet <- factor(diet, levels = unique(diet)[c(4, 1:3)])
+#life <- factor(life, levels = unique(life))
+#inter <- interaction(diet, life, drop = TRUE)
+#inter <- as.character(inter)
+#
+#rms <- which(inter == names(which(table(inter) == 1)))
+#sight <- sight[-rms, ]
+#
+#x <- stats::model.matrix( ~ mass + inter)
+#x <- x[-rms, ]
+#mass <- mass[-rms]
+##x <- x[, colSums(x) != 0]  
+## i think this is the correct thing to do because certain inter not obs
+#D <- ncol(x)
+#name.name <- colnames(x)
+#
+## phylogenetic data
+##vcv <- vcv(spt)
+##vcv <- vcv / max(diag(vcv))
+##U <- nrow(vcv)
+##id <- as.numeric(as.factor(occur$name.bi))
+#
+## climate data
+##u <- cbind(mean.o18, range.o18)
+##U <- ncol(u)
+## WARNING to include need to remove oldest bin!!!!
+#u <- cbind(temp.time.mean, temp.time.range)  # young to old
+#u <- u[-(nrow(u)), ]
+#u <- apply(u, 2, rev)  # old to young
+#
+#
+## make the plant phase indicator
+#mi.pl <- c(16, 2)
+#eo.mi <- c(50, 16)
+#pa.eo <- c(66, 50)
+#plants <- rbind(mi.pl, eo.mi, pa.eo)
+#
+#co.h <- seq(4, (ncol(sight) + 1) * 2, by = 2)
+#phase <- array(dim = length(co.h))
+#for(ii in seq(length(co.h))){
+#  phase[ii] <- which(plants[, 1] >= co.h[ii] & plants[, 2] < co.h[ii])
+#}
+#P <- 3
+#
+## fixed the reversed order
+#sight <- sight[, rev(seq(ncol(sight)))]  # from older to younger
+#phase <- factor(rev(phase)) # from older to younger
+#u <- model.matrix( ~ u + phase)
 #U <- ncol(u)
-# WARNING to include need to remove oldest bin!!!!
-u <- cbind(temp.time.mean, temp.time.range)  # young to old
-u <- u[-(nrow(u)), ]
-u <- apply(u, 2, rev)  # old to young
-
-
-# make the plant phase indicator
-mi.pl <- c(16, 2)
-eo.mi <- c(50, 16)
-pa.eo <- c(66, 50)
-plants <- rbind(mi.pl, eo.mi, pa.eo)
-
-co.h <- seq(4, (ncol(sight) + 1) * 2, by = 2)
-phase <- array(dim = length(co.h))
-for(ii in seq(length(co.h))){
-  phase[ii] <- which(plants[, 1] >= co.h[ii] & plants[, 2] < co.h[ii])
-}
-P <- 3
-
-# fixed the reversed order
-sight <- sight[, rev(seq(ncol(sight)))]  # from older to younger
-phase <- factor(rev(phase)) # from older to younger
-u <- model.matrix( ~ u + phase)
-U <- ncol(u)
-
-N <- nrow(sight)
-T <- ncol(sight)
-
-state <- as.numeric(factor(inter))
-state <- state[-rms]
-ufull <- u
-u <- u[-1, ]
-state <- mapvalues(state, 
-                   from = sort(unique(state)), 
-                   to = seq(length(unique(state))))
-D <- length(unique(state))
-
-# dump out the stan data
-stan_rdump(list = c('N', 'T', 'D', 'U', 
-                    'sight', 'state', 'u', 'ufull', 'mass'),
-           file = '../data/data_dump/trait_w_gaps.data.R')
+#
+#N <- nrow(sight)
+#T <- ncol(sight)
+#
+#state <- as.numeric(factor(inter))
+#state <- state[-rms]
+#ufull <- u
+#u <- u[-1, ]
+#state <- mapvalues(state, 
+#                   from = sort(unique(state)), 
+#                   to = seq(length(unique(state))))
+#D <- length(unique(state))
+#
+## dump out the stan data
+#stan_rdump(list = c('N', 'T', 'D', 'U', 
+#                    'sight', 'state', 'u', 'ufull', 'mass'),
+#           file = '../data/data_dump/trait_w_gaps.data.R')
