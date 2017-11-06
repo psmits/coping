3a6cbc151457c876a7c4dff821f113d8159c61ae
Peter Smits <thestinkypete@gmail.com>
2017-11-06 14:22:55 -0800

    fixed up tables to better reflect posterior

--- R/prob_calc.r
+++ R/prob_calc.r
@@ -8,13 +8,13 @@ library(xtable)
 #   diversity peaks/valleys vs average diversity
 
 # probability that group-level on origin is greater than 0
-# order is phase 3, temp mean, temp range, phase 2, phase 1
+# order is phase (Mi.Pl), Eo.Mi, Pa.Eo, temp
 out.origin <- out.surv <- list()
 for(ii in seq(nrow(ecotrans))) {
-  out.origin[[ii]] <- apply(ext2$o_gamma[, 2:3, ii], 2, function(x) 
-                            sum(x > 0) / length(x))
-  out.surv[[ii]] <- apply(ext2$s_gamma[, 2:3, ii], 2, function(x) 
-                          sum(x > 0) / length(x))
+  out.origin[[ii]] <- sum(ext2$o_gamma[, 3, ii] > 0) / 
+    length(ext2$o_gamma[, 3, ii])
+  out.surv[[ii]] <- sum(ext2$s_gamma[, 3, ii] > 0) / 
+    length(ext2$s_gamma[, 3, ii])
 }
 out.origin <- Reduce(rbind, out.origin)
 out.surv <- Reduce(rbind, out.surv)
@@ -28,7 +28,7 @@ rown <- apply(ecotrans, 1, function(x) Reduce(paste, rev(x)))
 
 rownames(out.origin) <- rownames(out.surv) <- rown
 colnames(out.origin) <- colnames(out.surv) <- 
-  c('P(gamma_{temp mean} > 0)', 'P(gamma_{temp range} > 0)')
+  c('P(gamma_{temp mean} > 0)')
 
 # make/print tables
 out.origin.tab <- xtable(out.origin, label = 'tab:origin_temp', digits = 3)
@@ -42,22 +42,18 @@ print.xtable(x = out.surv.tab, file = '../doc/surv_temp_raw.tex')
 # calculate differences amoungst plant phases
 plant.or <- plant.su <- list()
 for(ii in seq(nrow(ecotrans))) {
-  tt <- ext2$o_gamma[, c(1, 2, 3), ii]
-  tt[, 2] <- tt[, 2] + tt[, 1]
-  tt[, 3] <- tt[, 3] + tt[, 1]
+  tt <- ext2$o_gamma[, 1:2, ii]
   plantp <- c()
-  plantp[1] <- sum(tt[, 1] - tt[, 2] > 0) / nrow(tt)
-  plantp[2] <- sum(tt[, 2] - tt[, 3] > 0) / nrow(tt)
-  plantp[3] <- sum(tt[, 1] - tt[, 3] > 0) / nrow(tt)
+  plantp[1] <- sum(tt[, 1] > 0) / nrow(tt)
+  plantp[2] <- sum(tt[, 2] > 0) / nrow(tt)
+  plantp[3] <- sum(tt[, 1] - tt[, 2] > 0) / nrow(tt)
   plant.or[[ii]] <- plantp
 
-  tt <- ext2$s_gamma[, c(1, 2, 3), ii]
-  tt[, 2] <- tt[, 2] + tt[, 1]
-  tt[, 3] <- tt[, 3] + tt[, 1]
+  tt <- ext2$s_gamma[, 1:2, ii]
   plantp <- c()
-  plantp[1] <- sum(tt[, 1] - tt[, 2] > 0) / nrow(tt)
-  plantp[2] <- sum(tt[, 2] - tt[, 3] > 0) / nrow(tt)
-  plantp[3] <- sum(tt[, 1] - tt[, 3] > 0) / nrow(tt)
+  plantp[1] <- sum(tt[, 1] > 0) / nrow(tt)
+  plantp[2] <- sum(tt[, 2] > 0) / nrow(tt)
+  plantp[3] <- sum(tt[, 1] - tt[, 2] > 0) / nrow(tt)
   plant.su[[ii]] <- plantp
 }
 plant.or <- Reduce(rbind, plant.or)
@@ -65,7 +61,7 @@ plant.su <- Reduce(rbind, plant.su)
 
 rownames(plant.or) <- rownames(plant.su) <- rown
 colnames(plant.or) <- colnames(plant.su) <- 
-  c('P(Phase 1 > Phase 2)', 'P(Phase 2 > Phase 3)', 'P(Phase 1 > Phase 3)')
+  c('P(Eo.Mi > 0)', 'P(Pa.Eo > 0)', 'P(Eo.Mi > Pa.Eo)')
 
 plant.or.tab <- xtable(plant.or, label = 'tab:origin_plant', digits = 3)
 print.xtable(x = plant.or.tab, file = '../doc/origin_plant_raw.tex')
@@ -85,7 +81,11 @@ for(ii in seq(ncol(div.calc))) {
   pdiv[ii] <- sum(div.calc[, ii] > avg.div) / nrow(div.calc)
 }
 
-div.peaks <- data.frame(time = rev(sort(unique(diversity$time))), prob = pdiv)
+tm <- match(diversity$time, nalma$ma)
+diversity$nalma <- nalma$interval[tm]
+diversity$nalma <- factor(diversity$nalma, levels = nalma$interval)
+
+div.peaks <- data.frame(time = unique(diversity$nalma), prob = pdiv)
 names(div.peaks) <- c('Time (Mya)', 'P(N^{stand}_{t} > bar{N^{stand}})')
 div.peaks.tab <- xtable(div.peaks, label = 'tab:div_peak')
 print.xtable(x = div.peaks.tab, file = '../doc/div_peak_raw.tex')
@@ -135,7 +135,7 @@ for(ii in seq(ncol(growth.rate))) {
 }
 
 dl <- length(unique(diversity$time))
-rat.peaks <- data.frame(time = rev(sort(unique(diversity$time))[-dl]), prob = prate)
+rat.peaks <- data.frame(time = unique(diversity$nalma)[-dl], prob = prate)
 names(rat.peaks) <- c('Time (Mya)', 'P(D^{rate}_{t} > bar{D^{rate}})')
 rat.peaks.tab <- xtable(rat.peaks, label = 'tab:rate_peak')
 print.xtable(x = rat.peaks.tab, file = '../doc/rate_peak_raw.tex')
