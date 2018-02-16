cec86e638ec1e5b7a4b735a585840d63fff86ee6
Peter Smits <thestinkypete@gmail.com>
2018-02-16 11:13:09 -0800

    running for real. so slow it hurts

--- src/trait_model_wrap_fast.sh
+++ src/trait_model_wrap_fast.sh
@@ -3,7 +3,7 @@ for i in `seq 1 4`;
 do
   ../stan/turnover_rwprior sample \
     adapt delta=0.9 \
-    num_samples=1000 num_warmup=1000 \
+    num_samples=5000 num_warmup=5000 thin=5 \
     algorithm=hmc engine=nuts max_depth=10 \
     id=$i \
     init=0 \
